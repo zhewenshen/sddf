a402ee1886c3d09dc5389ca0dea3754cdfd7e77b
zhewen shen <zhewen.shen@outlook.com>
2025-02-20 16:53:13 +1100

    remove header files

--- network/components/virt_tx_ccomp.c
+++ network/components/virt_tx_ccomp.c
@@ -1,4 +1,24 @@
-#include "virt_tx_ccomp.h"
+#include <sddf/network/queue.h>
+#include <sddf/network/config.h>
+
+extern net_virt_tx_config_t config;
+
+typedef unsigned int microkit_channel;
+
+extern void _ccomp_microkit_notify(microkit_channel ch);
+extern void _ccomp_microkit_deferred_notify(microkit_channel ch);
+extern void _ccomp_assert(bool condition);
+extern void _ccomp_cache_clean_and_invalidate(unsigned long start, unsigned long end);
+extern int _ccomp_net_enqueue_active(net_queue_handle_t *queue, net_buff_desc_t *buffer);
+extern void _ccomp_tx_provide_sddf_dprintf(unsigned long offset);
+extern int _ccomp_net_enqueue_free(net_queue_handle_t *queue, net_buff_desc_t *buffer);
+extern void _ccomp_cache_clean(unsigned long start, unsigned long end);
+typedef struct state {
+    net_queue_handle_t tx_queue_drv;
+    net_queue_handle_t tx_queue_clients[SDDF_NET_MAX_CLIENTS];
+} state_t;
+
+state_t state;
 
 int virt_tx_ccomp_extract_offset(uintptr_t *phys)
 {
