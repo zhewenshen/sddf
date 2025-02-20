c345c9a6cb55f3d03dcc8aeccada0d5922bb2f3d
zhewen shen <zhewen.shen@outlook.com>
2025-02-20 13:59:16 +1100

    wip: compcert imx ethernet driver

---
+++ drivers/network/imx/ethernet_ccomp.c
@@ -0,0 +1,101 @@
+#include "ethernet_ccomp.h"
+
+void ethernet_ccomp_init_hw_ring(hw_ring_t *ring, volatile struct descriptor *descr, uint32_t capacity) {
+    ring->descr = descr;
+    ring->capacity = capacity;
+    ring->head = 0;
+    ring->tail = 0;
+}
+
+bool ethernet_ccomp_hw_ring_full(hw_ring_t *ring) {
+    return ring->tail - ring->head == ring->capacity;
+}
+
+bool ethernet_ccomp_hw_ring_empty(hw_ring_t *ring) {
+    return ring->tail - ring->head == 0;
+}
+
+void ethernet_ccomp_update_ring_slot(hw_ring_t *ring, unsigned int idx, uintptr_t phys, uint16_t len, uint16_t stat) {
+    volatile struct descriptor *d = &(ring->descr[idx]);
+
+    d->addr = phys;
+    d->len = len;
+
+    _ccomp_thread_memory_release(); // double check this
+
+    d->stat = stat;
+}
+
+void ethernet_ccomp_rx_provide(void)
+{
+    bool reprocess = true;
+    while (reprocess) {
+        while (!ethernet_ccomp_hw_ring_full(&rx) && !net_queue_empty_free(&rx_queue)) {
+            net_buff_desc_t buffer;
+            int err = net_dequeue_free(&rx_queue, &buffer);
+            // we can ignore asserts for now...
+            // assert(!err); 
+            _ccomp_assert(!err);
+
+            uint32_t idx = rx.tail % rx.capacity;
+            uint16_t stat = RXD_EMPTY;
+            if (idx + 1 == rx.capacity) {
+                stat |= WRAP;
+            }
+            rx.descr_mdata[idx] = buffer;
+            ethernet_ccomp_update_ring_slot(&rx, idx, buffer.io_or_offset, 0, stat);
+            rx.tail++;
+            eth->rdar = RDAR_RDAR;
+        }
+
+        /* Only request a notification from virtualiser if HW ring not full */
+        if (!ethernet_ccomp_hw_ring_full(&rx)) {
+            net_request_signal_free(&rx_queue);
+        } else {
+            net_cancel_signal_free(&rx_queue);
+        }
+        reprocess = false;
+
+        if (!net_queue_empty_free(&rx_queue) && !ethernet_ccomp_hw_ring_full(&rx)) {
+            net_cancel_signal_free(&rx_queue);
+            reprocess = true;
+        }
+    }
+}
+
+void ethernet_ccomp_rx_return(void)
+{
+    bool packets_transferred = false;
+    while (!ethernet_ccomp_hw_ring_empty(&rx)) {
+        /* If buffer slot is still empty, we have processed all packets the device has filled */
+        uint32_t idx = rx.head % rx.capacity;
+        volatile struct descriptor *d = &(rx.descr[idx]);
+        if (d->stat & RXD_EMPTY) {
+            break;
+        }
+
+        // THREAD_MEMORY_ACQUIRE();
+        _ccomp_thread_memory_acquire();
+
+        net_buff_desc_t buffer = rx.descr_mdata[idx];
+        buffer.len = d->len;
+
+        // ccomp dont support struct as function parameter... this is so sad...
+        // int err = net_enqueue_active(&rx_queue, buffer);
+        int err = _ccomp_net_enqueue_active(&rx_queue, &buffer);
+        // assert(!err);
+        _ccomp_assert(!err);
+
+        packets_transferred = true;
+        rx.head++;
+    }
+
+    // how do we deal with this?
+    if (packets_transferred && net_require_signal_active(&rx_queue)) {
+        net_cancel_signal_active(&rx_queue);
+        
+        // microkit_notify(config.virt_rx.id);
+        _ccomp_microkit_notify(config.virt_rx.id);
+    }
+}
+
---
+++ drivers/network/imx/ethernet_ccomp.h
@@ -0,0 +1,72 @@
+#ifndef ETHERNET_CCOMP_H
+#define ETHERNET_CCOMP_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <sddf/network/queue.h>
+#include <sddf/network/config.h>
+#include <sddf/resources/device.h>
+
+#include "ethernet.h"
+
+typedef unsigned int microkit_channel;
+
+extern device_resources_t device_resources;
+extern net_driver_config_t config;
+
+#define RX_COUNT 256
+#define TX_COUNT 256
+#define MAX_COUNT 256
+
+#define MAX_PACKET_SIZE     1536
+
+// the microkit stuff we need
+extern void _ccomp_microkit_notify(microkit_channel ch);
+extern void _ccomp_microkit_deferred_irq_ack(microkit_channel ch);
+
+// some other stuff we need
+extern void _ccomp_thread_memory_release(void);
+extern void _ccomp_thread_memory_acquire(void);
+extern void _ccomp_assert(bool condition);
+
+// sddf stuff
+extern int _ccomp_net_enqueue_active(net_queue_handle_t *queue, net_buff_desc_t *buffer);
+extern void _ccomp_handle_irq_sddf_dprintf(unsigned int e);
+extern void _ccomp_notified_sddf_dprintf(microkit_channel ch);
+
+/* HW ring descriptor (shared with device) */
+struct descriptor {
+    uint16_t len;
+    uint16_t stat;
+    uint32_t addr;
+};
+
+/* HW ring buffer data type */
+typedef struct {
+    uint32_t tail; /* index to insert at */
+    uint32_t head; /* index to remove from */
+    uint32_t capacity; /* capacity of the ring */
+    volatile struct descriptor *descr; /* buffer descriptor array */
+    net_buff_desc_t descr_mdata[MAX_COUNT]; /* associated meta data array */
+} hw_ring_t;
+
+hw_ring_t rx; /* Rx NIC ring */
+hw_ring_t tx; /* Tx NIC ring */
+
+net_queue_handle_t rx_queue;
+net_queue_handle_t tx_queue;
+
+volatile struct enet_regs *eth;
+
+// juice
+void ethernet_ccomp_init_hw_ring(hw_ring_t *ring, volatile struct descriptor *descr, uint32_t capacity);
+void ethernet_ccomp_update_ring_slot(hw_ring_t *ring, unsigned int idx, uintptr_t phys, uint16_t len, uint16_t stat);
+bool ethernet_ccomp_hw_ring_full(hw_ring_t *ring);
+bool ethernet_ccomp_hw_ring_empty(hw_ring_t *ring);
+void ethernet_ccomp_rx_provide(void);
+void ethernet_ccomp_rx_return(void);
+void ethernet_ccomp_tx_provide(void);
+void ethernet_ccomp_tx_return(void);
+
+
+#endif // ETHERNET_CCOMP_H
