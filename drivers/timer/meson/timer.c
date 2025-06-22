e8d2200e1effb4bb37ad9cadf0144d479264ec8d
zhewen shen <zhewen.shen@outlook.com>
2025-06-22 23:06:02 +1000

    i hate timers and big numbers

--- drivers/timer/meson/timer.c
+++ drivers/timer/meson/timer.c
@@ -11,6 +11,10 @@
 
 #define MAX_TIMEOUTS 6
 
+// Workaround for Pancake 64-bit comparison bug: use a large but safe value
+// instead of UINT64_MAX to represent "no timeout set"
+#define TIMEOUT_INVALID_VALUE 9999999999999999ULL
+
 #define TIMER_REG_START   0x70    // TIMER_MUX
 
 #define TIMER_A_INPUT_CLK 0
@@ -54,6 +58,37 @@ volatile struct timer_regs *regs;
  * indexed by client ID. */
 static uint64_t timeouts[MAX_TIMEOUTS];
 
+// Pancake runtime support
+static char cml_memory[1024*20];
+extern void *cml_heap;
+extern void *cml_stack;
+extern void *cml_stackend;
+
+extern void cml_main(void);
+
+void cml_exit(int arg) {
+    microkit_dbg_puts("ERROR! We should not be getting here\n");
+}
+
+void cml_err(int arg) {
+    if (arg == 3) {
+        microkit_dbg_puts("Memory not ready for entry. You may have not run the init code yet, or be trying to enter during an FFI call.\n");
+    }
+    cml_exit(arg);
+}
+
+void cml_clear() {
+    microkit_dbg_puts("Trying to clear cache\n");
+}
+
+void init_pancake_mem() {
+    unsigned long cml_heap_sz = 1024*10;
+    unsigned long cml_stack_sz = 1024*10;
+    cml_heap = cml_memory;
+    cml_stack = cml_heap + cml_heap_sz;
+    cml_stackend = cml_stack + cml_stack_sz;
+}
+
 static uint64_t get_ticks(void)
 {
     uint64_t initial_high = regs->timer_e_hi;
@@ -69,40 +104,32 @@ static uint64_t get_ticks(void)
 
 static void process_timeouts(uint64_t curr_time)
 {
+    uintptr_t *pnk_mem = (uintptr_t *) cml_heap;
+    
     for (int i = 0; i < MAX_TIMEOUTS; i++) {
         if (timeouts[i] <= curr_time) {
             microkit_notify(i);
-            timeouts[i] = UINT64_MAX;
+            timeouts[i] = TIMEOUT_INVALID_VALUE;
+            // update Pancake memory
+            pnk_mem[10 + i] = TIMEOUT_INVALID_VALUE;
         }
     }
 
-    uint64_t next_timeout = UINT64_MAX;
+    uint64_t next_timeout = TIMEOUT_INVALID_VALUE;
     for (int i = 0; i < MAX_TIMEOUTS; i++) {
         if (timeouts[i] < next_timeout) {
             next_timeout = timeouts[i];
         }
     }
 
-    if (next_timeout != UINT64_MAX) {
+    if (next_timeout != TIMEOUT_INVALID_VALUE) {
         regs->mux &= ~TIMER_A_MODE;
         regs->timer_a = next_timeout - curr_time;
         regs->mux |= TIMER_A_EN;
     }
 }
 
-void notified(microkit_channel ch)
-{
-    if (ch != device_resources.irqs[0].id) {
-        sddf_dprintf("TIMER DRIVER|LOG: unexpected notification from channel %u\n", ch);
-        return;
-    }
 
-    microkit_deferred_irq_ack(ch);
-    regs->mux &= ~TIMER_A_EN;
-
-    uint64_t curr_time = get_ticks();
-    process_timeouts(curr_time);
-}
 
 seL4_MessageInfo_t protected(microkit_channel ch, microkit_msginfo msginfo)
 {
@@ -114,8 +141,14 @@ seL4_MessageInfo_t protected(microkit_channel ch, microkit_msginfo msginfo)
     }
     case SDDF_TIMER_SET_TIMEOUT: {
         uint64_t curr_time = get_ticks();
-        uint64_t offset_us = seL4_GetMR(0) / NS_IN_US;
+        uint64_t offset_ns = seL4_GetMR(0);
+        uint64_t offset_us = offset_ns / NS_IN_US;
         timeouts[ch] = curr_time + offset_us;
+        
+        // update Pancake memory
+        uintptr_t *pnk_mem = (uintptr_t *) cml_heap;
+        pnk_mem[10 + ch] = timeouts[ch];
+        
         process_timeouts(curr_time);
         break;
     }
@@ -135,7 +168,7 @@ void init(void)
     assert(device_resources.num_regions == 1);
 
     for (int i = 0; i < MAX_TIMEOUTS; i++) {
-        timeouts[i] = UINT64_MAX;
+        timeouts[i] = TIMEOUT_INVALID_VALUE;
     }
 
     regs = (void *)((uintptr_t)device_resources.regions[0].region.vaddr + TIMER_REG_START);
@@ -145,4 +178,21 @@ void init(void)
                 (TIMEOUT_TIMEBASE_1_US << TIMER_A_INPUT_CLK);
 
     regs->timer_e = 0;
+
+    init_pancake_mem();
+    
+    uintptr_t *pnk_mem = (uintptr_t *) cml_heap;
+    
+    pnk_mem[0] = device_resources.irqs[0].id;  // IRQ_CH
+    pnk_mem[1] = (uintptr_t)regs;  // TIMER_REG_BASE
+    
+    for (int i = 0; i < MAX_TIMEOUTS; i++) {
+        pnk_mem[10 + i] = TIMEOUT_INVALID_VALUE;
+    }
+    
+    cml_main();
 }
+
+// Pancake will provide the notified function
+extern void notified(microkit_channel ch);
+
--- drivers/timer/meson/timer_driver.mk
+++ drivers/timer/meson/timer_driver.mk
@@ -4,24 +4,44 @@
 # SPDX-License-Identifier: BSD-2-Clause
 #
 # Include this snippet in your project Makefile to build
-# the Meson timer driver
+# the Meson timer driver with Pancake support
 #
 # NOTES:
 #  Generates timer_driver.elf
 #  Expects libsddf_util_debug.a in ${LIBS}
+#  Requires CAKE_COMPILER to be set for Pancake compilation
 
 TIMER_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
-timer_driver.elf: timer/timer.o
-	$(LD) $(LDFLAGS) $< $(LIBS) -o $@
+# Set up clang target flags if using clang
+ifeq ($(CC),clang)
+    CLANG_TARGET_FLAGS := -target aarch64-none-elf
+else
+    CLANG_TARGET_FLAGS :=
+endif
 
-timer/timer.o: ${TIMER_DIR}/timer.c  ${CHECK_FLAGS_BOARD_MD5} |timer
+# Pancake source files
+TIMER_PNK = ${UTIL}/util.🥞 \
+	${TIMER_DIR}/timer.🥞
+
+timer_driver.elf: timer/timer_pnk.o timer/timer.o pancake_ffi.o libsddf_util_debug.a
+	$(LD) $(LDFLAGS) $^ $(LIBS) -o $@
+
+timer/timer.o: ${TIMER_DIR}/timer.c ${CHECK_FLAGS_BOARD_MD5} |timer
 	${CC} ${CFLAGS} -o $@ -c $<
+
+timer/timer_pnk.o: timer/timer_pnk.S
+	$(CC) $(CLANG_TARGET_FLAGS) -c -mcpu=$(CPU) $< -o $@
+
+timer/timer_pnk.S: $(TIMER_PNK) | timer
+	cat $(TIMER_PNK) | cpp -P | $(CAKE_COMPILER) --target=arm8 --pancake --main_return=true > $@
 
 timer:
 	mkdir -p timer
 
 clean::
 	rm -rf timer
+	rm -f timer/timer_pnk.[So]
+
 clobber::
 	rm -f timer_driver.elf
